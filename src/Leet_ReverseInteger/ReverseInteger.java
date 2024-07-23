bfd9d31a612f8d8559579ba762a305f2231c858e
wssong1309 <wssong1309@gmail.com>
2024-07-23 20:06:08 +0900

    LeetCode - Reversed Integer

---
+++ src/Leet_ReverseInteger/ReverseInteger.java
@@ -0,0 +1,27 @@
+package Leet_ReverseInteger;
+
+import java.util.stream.Stream;
+
+public class ReverseInteger {
+    public static int reverse(int x) {
+        long reversedNum = 0;
+
+        while (x != 0) {
+            int lastDigit = x % 10;
+            reversedNum += lastDigit;
+            reversedNum = reversedNum * 10;
+            x = x / 10;
+        }
+        reversedNum = reversedNum / 10;
+
+        if (reversedNum > Integer.MAX_VALUE || reversedNum < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        if (x < 0) {
+            return (int) (-1 * reversedNum);
+        }
+
+        return (int) reversedNum;
+    }
+}
